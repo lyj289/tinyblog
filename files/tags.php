ee055b8139eb45edf16fdf895ec5d74b92babba6
lyj289 <lyj289@163.com>
2019-12-22 00:20:49 +0800

    Make Blog theme more simple

--- files/tags.php
+++ files/tags.php
@@ -332,23 +332,6 @@ function mc_the_short_content($print = true) {
   }
   return strip_tags($html);
 }
-function mc_the_md() {
-  global $mc_data;
-  if (!isset($mc_data)) {
-    global $mc_post_id;
-    $data = unserialize(file_get_contents('files/posts/data/'.$mc_post_id.'.dat'));
-    $html = isset($data['md']);
-  }
-  else {
-    $html = isset($mc_data['md']);
-  }
-  if ($html){
-    echo "has md";
-  } else {
-    echo "no md";
-  }
-
-}
 
 function mc_the_link() {
   global $mc_post_id, $mc_post, $mc_config;
@@ -386,4 +369,36 @@ function mc_comment_code() {
   global $mc_config;
   echo isset($mc_config['comment_code']) ? $mc_config['comment_code'] : '';
 }
+function isMobile() {
+  // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+  if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) {
+    return true;
+  }
+  // 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+  if (isset($_SERVER['HTTP_VIA'])) {
+    // 找不到为flase,否则为true
+    return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
+  }
+  // 脑残法，判断手机发送的客户端标志,兼容性有待提高。其中'MicroMessenger'是电脑微信
+  if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $clientkeywords = array('nokia','sony','ericsson','mot','samsung','htc','sgh','lg','sharp','sie-','philips','panasonic','alcatel',
+    'lenovo','iphone','ipod','blackberry','meizu','android','netfront','symbian','ucweb','windowsce','palm','operamini','operamobi',
+    'openwave','nexusone','cldc','midp','wap','mobile','MicroMessenger');
+    // 从HTTP_USER_AGENT中查找手机浏览器的关键字
+    if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
+      return true;
+    }
+  }
+  // 协议法，因为有可能不准确，放到最后判断
+  if (isset ($_SERVER['HTTP_ACCEPT'])) {
+    // 如果只支持wml并且不支持html那一定是移动设备
+    // 如果支持wml和html但是wml在html之前则是移动设备
+    if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') ===
+    false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
+      return true;
+    }
+  }
+  return false;
+}
+
 ?>
--- files/theme/v/index.php
+++ files/theme/v/index.php
@@ -2,15 +2,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta charset=utf-8>
+<meta name=viewport content="width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover">
 <meta name="description" content="<?php mc_the_title();?>" />
 <title><?php if (mc_is_post() || mc_is_page()) { mc_the_title(); ?> | <?php mc_site_name(); } else { mc_site_name(); ?> | <?php mc_site_desc(); }?></title>
 <meta name="keywords" content="<?php mc_the_onlytags(); ?>"/>
-<link rel="stylesheet" type="text/css" href="http://localhost/github/editor-md/css/editormd.css">
+<link rel="stylesheet" type="text/css" href="/github/editor-md/css/editormd.css">
 <link href="<?php mc_theme_url('style.css'); ?>" type="text/css" rel="stylesheet"/>
 
 </head>
-<body class="">
+<body class="skin9<?php if(isMobile()) echo ' mobile'; ?>">
 <div id="main">
   <div id="header">
     <div id="sitename">
@@ -21,17 +22,6 @@
       <a href="<?php mc_site_link(); ?>/?archive/" class="archive" title="文章">文章</a>
       <a href="<?php mc_site_link(); ?>/?rss/" class="rss" title="RSS订阅" target="_blank">RSS订阅</a>
     </div>
-    <div id="skinbar">
-      <a href="#" title="" class="skin1" onclick="switcherSkin(this)"></a>
-      <a href="#" title="" class="skin2" onclick="switcherSkin(this)"></a>
-      <a href="#" title="" class="skin3" onclick="switcherSkin(this)"></a>
-      <a href="#" title="" class="skin4" onclick="switcherSkin(this)"></a>
-      <a href="#" title="" class="skin5" onclick="switcherSkin(this)"></a>
-      <a href="#" title="" class="skin6" onclick="switcherSkin(this)"></a>
-      <a href="#" title="" class="skin7" onclick="switcherSkin(this)"></a>
-      <a href="#" title="" class="skin8" onclick="switcherSkin(this)"></a>
-      <a href="#" title="" class="skin9" onclick="switcherSkin(this)"></a>
-    </div>
   </div>
   <div class="clear"></div>
   <div id="content">
@@ -39,6 +29,11 @@
       <?php if (mc_is_post()) { ?>
       <div class="post">
         <h1 class="title"><?php mc_the_link(); ?></h1>
+        <div class="post_meta">
+          <div class="post_date"><?php mc_the_date(); ?></div>
+          <div class="post_tag"><?php mc_the_tags('','',''); ?></div>
+        </div>
+        <hr class="page-break editormd-page-break">
         <div class="content" style="min-height:300px;" id="post_content">
           <?php if (mc_is_md()) { ?>
             <textarea id="md_content" style="display:none;"><?php mc_the_content(); ?></textarea>
@@ -46,11 +41,7 @@
             <?php mc_the_content(); ?>
           <?php }?>
         </div>
-        <div class="post_meta">
-          <div class="post_date"><?php mc_the_date(); ?></div>
-          <div class="post_tag"><?php mc_the_tags('','',''); ?></div>
-          <div class="post_comm"><a href="<?php mc_post_link(); ?>#comm">评论</a></div>
-        </div>
+
       </div>
         <?php if (mc_can_comment()) { ?>
         <div id="comm">
@@ -66,9 +57,9 @@
           <?php mc_the_content(); ?>
         </div>
       </div>
-      <?php if (mc_can_comment()) { ?>
-      <div id="comm"><?php mc_comment_code(); ?></div>
-      <?php } ?>
+        <?php if (mc_can_comment()) { ?>
+        <div id="comm"><?php mc_comment_code(); ?></div>
+        <?php } ?>
       <?php } else if (mc_is_archive()) { ?>
       <div class="post">
         <h1 class="title">文章存档</h1>
@@ -78,11 +69,11 @@
                     <tr>
                         <td width="160" style="vertical-align:top;">
                             <h1 class="date_list">月份</h1>
-                            <ul id="list"><?php mc_date_list(); ?></ul>
+                            <ul class="list"><?php mc_date_list(); ?></ul>
                         </td>
                         <td width="500" style="vertical-align:top;">
                             <h1 class="tag_list">标签</h1>
-                            <ul id="list"><?php mc_tag_list(); ?></ul>
+                            <ul class="list"><?php mc_tag_list(); ?></ul>
                         </td>
                     </tr>
                 </tbody>
@@ -95,21 +86,18 @@
       <?php } else if (mc_is_date()) { ?>
       <div id="page_info"><span><?php mc_date_name(); ?></span></div>
       <?php } ?>
+      <h3>共 <?php echo $mc_post_count; ?> 篇</h3>
       <?php while (mc_next_post()) { ?>
-      <div class="post">
-        <h1 class="title"><?php mc_the_link(); ?></h1>
-        <div class="content">
-          <!-- todo: 添加是否通过markdown解析，来返回不同的内容解析方法 -->
-          <p>
-            <?php mc_the_short_content(true); ?>
-          </p>
-        </div>
-        <div class="post_meta">
-          <div class="post_date"><?php mc_the_date(); ?></div>
-          <div class="post_tag"><?php mc_the_tags('','',''); ?></div>
-          <div class="post_comm"><a href="<?php mc_post_link(); ?>#comm">评论</a></div>
-        </div>
-      </div>
+
+      <p>
+        <strong>
+          <?php mc_the_link(); ?>
+        </strong>
+
+        [ <?php mc_the_date(); ?> ]
+        [ <?php mc_the_tags('','',''); ?> ]
+      </p>
+
       <?php } ?>
       <div class="clear"></div>
 
@@ -121,14 +109,19 @@
         <?php if (mc_has_new()) { ?><?php mc_goto_new('&lt;-'); ?><?php } ?>
         <?php if (mc_has_old()) { ?><?php mc_goto_old('-&gt;'); ?><?php } ?>
   </div>
+
+<div class="footer">李玉俭----2012-11-02修改 <?php mc_site_desc();?></div>
+<script type="text/javascript" src="/blog/admin/xheditor/jquery.js"></script>
+<script type="text/javascript" src="/github/editor-md/lib/marked.min.js"></script>
+<script type="text/javascript" src="/github/editor-md/lib/prettify.min.js"></script>
+<script type="text/javascript" src="/github/editor-md/lib/underscore.min.js"></script>
+<script type="text/javascript" src="/github/editor-md/lib/flowchart.min.js"></script>
+<script type="text/javascript" src="/github/editor-md/lib/jquery.flowchart.min.js"></script>
+<script type="text/javascript" src="/github/editor-md/lib/raphael.min.js"></script>
+<script type="text/javascript" src="/github/editor-md/lib/sequence-diagram.min.js"></script>
+<script type="text/javascript" src="/github/editor-md/editormd.min.js"></script>
 <script>
   window.onload = function(){
-    if (window.localStorage) {
-      var ls = localStorage;
-      var s = ls.getItem('body-skin');
-      document.querySelectorAll('body')[0].className = s;
-      document.querySelectorAll('#skinbar a.' + s)[0].className += ' cur';
-    }
     <?php if (mc_is_md()) { ?>
     testEditormdView = editormd.markdownToHTML("post_content", {
         markdown        :  $("#md_content").text(), // "\r\n" +
@@ -182,15 +175,5 @@
       }
   }
 </script>
-<div class="footer">李玉俭----2012-11-02修改 <?php mc_site_desc();?></div>
-<script type="text/javascript" src="http://localhost/blog/admin/xheditor/jquery.js"></script>
-<script type="text/javascript" src="http://localhost/github/editor-md/lib/marked.min.js"></script>
-<script type="text/javascript" src="http://localhost/github/editor-md/lib/prettify.min.js"></script>
-<script type="text/javascript" src="http://localhost/github/editor-md/lib/underscore.min.js"></script>
-<script type="text/javascript" src="http://localhost/github/editor-md/lib/flowchart.min.js"></script>
-<script type="text/javascript" src="http://localhost/github/editor-md/lib/jquery.flowchart.min.js"></script>
-<script type="text/javascript" src="http://localhost/github/editor-md/lib/raphael.min.js"></script>
-<script type="text/javascript" src="http://localhost/github/editor-md/lib/sequence-diagram.min.js"></script>
-<script type="text/javascript" src="http://localhost/github/editor-md/editormd.min.js"></script>
 </body>
 </html>
